a936b3ae3cec67a20306aab42823e82a738bf580
petermlvyn <petermlvyn@gmail.com>
2026-07-05 18:40:51 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
+st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="", 
+layout="wide")
+
+st.title("Smart Hospital Patient Navigator")
